310564b9492891303bec7ef0153ab8ec3a2ee861
Raed Shomali <shomali11@gmail.com>
2017-06-04 19:53:23 -0400

    Small clean up

--- pretty_json.go
+++ pretty_json.go
@@ -12,10 +12,10 @@ const (
 
 func PrettyJson(data interface{}) (string, error) {
 	buffer := new(bytes.Buffer)
-	enc := json.NewEncoder(buffer)
-	enc.SetIndent(empty, tab)
+	encoder := json.NewEncoder(buffer)
+	encoder.SetIndent(empty, tab)
 
-	err := enc.Encode(data)
+	err := encoder.Encode(data)
 	if err != nil {
 		return empty, err
 	}
